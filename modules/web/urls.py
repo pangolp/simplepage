692a121b79733bef4a83281e39aedacbec978d80
Pagani Walter <paganiwalter@gmail.com>
2019-05-02 14:30:26 -0300

    agregando mas sitios

--- modules/web/urls.py
+++ modules/web/urls.py
@@ -2,11 +2,17 @@ from django.urls import path
 from .views import (
 	IndexView,
 	InstitucionalView,
+	GaleriaView,
+	TeoriaView,
+	DocentesView,
 )
 
 
 app_name = 'web'
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('institucional/', InstitucionalView.as_view(), name='institucional'),
+    path('institucional/autoridades/', InstitucionalView.as_view(), name='institucional'),
+    path('galeria/', GaleriaView.as_view(), name='galeria'),
+    path('teoria/', TeoriaView.as_view(), name='teoria'),
+    path('docentes/', DocentesView.as_view(), name='docentes'),
 ]
--- modules/web/views.py
+++ modules/web/views.py
@@ -8,3 +8,15 @@ class IndexView(TemplateView):
 
 class InstitucionalView(TemplateView):
 	template_name = 'web/institucional.html'
+
+
+class GaleriaView(TemplateView):
+	template_name = 'web/galeria.html'
+
+
+class TeoriaView(TemplateView):
+	template_name = 'web/teoria.html'
+
+
+class DocentesView(TemplateView):
+	template_name = 'web/docentes.html'
